acf1a8a79456a10007746a5513d2db26f6216ade
Michael Christenson II <m3talsmith@gmail.com>
2010-01-20 18:06:19 -0500

    devise installed

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,12 @@
 class Person
   include MongoMapper::Document
-
+  devise :authenticatable, :recoverable, :rememberable
+  
   key :login, String
   key :encrypted_password, String
   key :password_salt, String
   key :reset_password_token, String
   key :remember_token, String
-  key :activated, Boolean
-  key :activated_at, Time
-  key :activate_token, String
   timestamps!
   userstamps!
   
