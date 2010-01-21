73ca51721c2338c603c15b0086160a317d018b56
Michael Christenson II <m3talsmith@gmail.com>
2010-01-20 23:08:48 -0500

    added bare planning structure

--- test/factories/people.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :person do |person|
-  person.login { 'string' }
-
-  person.created_at { Time.now.to_s(:db) }
-  person.updated_at { Time.now.to_s(:db) }
-end
--- test/unit/person_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PersonTest < ActiveSupport::TestCase
-  should "be valid with factory" do
-    assert_valid Factory.build(:person)
-  end
-end
